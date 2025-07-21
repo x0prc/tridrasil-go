73fac89a0ac754df199da50a85f4da70b80892e0
x0prc <c0ldheat@protonmail.com>
2025-07-21 23:06:54 +0530

    feat : provide initial working for link quality plugin

---
+++ plugins/linkquality/linkquality.go
@@ -0,0 +1,36 @@
+package linkquality
+
+import (
+	"sync"
+	"time"
+
+	"/types"
+)
+
+type PeerLink struct {
+	NodeID  types.NodeID
+	Metrics types.LinkMetrics
+	mu      sync.Mutex
+}
+
+func (pl *PeerLink) UpdateMetrics(latency time.Duration, packetLoss float64) {
+	pl.mu.Lock()
+	defer pl.mu.Unlock()
+	pl.Metrics.Latency = latency
+	pl.Metrics.PacketLoss = packetLoss
+	pl.Metrics.LastUpdate = time.Now()
+}
+
+func (pl *PeerLink) GetMetrics() types.LinkMetrics {
+	pl.mu.Lock()
+	defer p1.mu.Unlock()
+	return p1.Metrics
+}
+
+func CalculatePathCost(latency time.Duration, packetLoss float64, hops int)
+float64 {
+	alpha := 1.0
+	beta := 10.0
+	gamma := 0.5
+	return alpha*latency.Seconds() + beta*packetLoss + gamma*float64(hops)
+}
